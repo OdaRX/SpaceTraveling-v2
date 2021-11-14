84dc1cb3134c254f8431aba589fa922bdf72b317
unknown <chromegold70@gmail.com>
2021-11-14 05:36:28 -0300

    Add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
+import styles from './header.module.scss';
+import commonStyles from '../../styles/common.module.scss';
+import Link from 'next/link';
+
 export default function Header() {
-  // TODO
+  return (
+    <>
+      <div className={`${commonStyles.container} ${styles.content}`}>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="spacetraveling" />
+          </a>
+        </Link>
+      </div>
+    </>
+  );
 }
